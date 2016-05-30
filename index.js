be7638bee1ebc03f2b7f0b3bccad579fcad502e6
jenylu <jenniferlu39@gmail.com>
2016-05-30 15:26:55 -0700

    sentiment color for individual word selection added

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function showWordCount(technique) {
 	        	var div = document.getElementById('bm_' + year);
 		        //console.log(div.innerHTML);
 
-		        $('#bm_' + year).append('<span onclick="showThisWord(this.innerHTML)" class="word ' + new_word +'">['+word+"] "+'</span>');
+		        $('#bm_' + year).append('<span onclick="showThisWord(this.innerHTML, ' + sentiment_scale + ')" class="word ' + new_word +'">['+word+"] "+'</span>');
 		        //div.innerHTML = div.innerHTML + word + "\n";
 		    }
 	    }
@@ -354,7 +354,7 @@ $(".legend.year").on("click", function() {
 //     showWordCount(selectedTechnique);
 // }); 
 
-function showThisWord(word) {
+function showThisWord(word, sentimentVal) {
 	console.log(word);
 	word = word.toString().substring(1, word.length - 2);
 	console.log(word);
@@ -377,14 +377,77 @@ function showThisWord(word) {
 	var selected_categories = document.getElementsByClassName("selectedCol");
     console.log(selected_categories);
 
+    var sentimentColor = calcSentimentColor(sentimentVal);
+
     for (i = 0; i < selected_categories.length; i ++) {
     	cur_category = selected_categories[i].innerHTML.toLowerCase();
     	console.log(cur_category);
     	$(".item." + cur_category).css('color', '#D3D3D3');
     	$('.' + cur_category + ' > .word').css('color', '#D3D3D3');
-    	$('.' + cur_category + ' > .word.' + new_word).css('color', 'black');
+    	$('.' + cur_category + ' > .word.' + new_word).css('color', sentimentColor);
     }
 
 
 }
 
+function calcSentimentColor(sentimentVal) {
+    console.log("ORIG SENTIMENT: " + sentimentVal);
+    sentimentVal = sentimentVal * 50.0 + 50.0;  //convert sentiment value to scale 0-100
+    console.log("converted SENTIMENT: " + sentimentVal);
+
+    //convert sentiment scale into a gradient color from red to green
+    if (sentimentVal > 100) {
+        sentimentVal = 100;
+    }
+    else if (sentimentVal < 0) {
+        val = 0;
+    }
+    // $(".value", span).text(val);
+    
+    var h= Math.floor((sentimentVal) * 120 / 100);
+    var s = Math.abs(sentimentVal - 50)/50;
+    var v = 1;
+
+    //convert to color black
+    if (s == 0) { v = 0; }
+    
+    var sentimentColor = hsv2rgb(h, s, v);
+    console.log("COLOR " + sentimentColor);
+
+    return sentimentColor;
+}
+
+var hsv2rgb = function(h, s, v) {
+  // adapted from http://schinckel.net/2012/01/10/hsv-to-rgb-in-javascript/
+  var rgb, i, data = [];
+  if (s === 0) {
+    rgb = [v,v,v];
+  } else {
+    h = h / 60;
+    i = Math.floor(h);
+    data = [v*(1-s), v*(1-s*(h-i)), v*(1-s*(1-(h-i)))];
+    switch(i) {
+      case 0:
+        rgb = [v, data[2], data[0]];
+        break;
+      case 1:
+        rgb = [data[1], v, data[0]];
+        break;
+      case 2:
+        rgb = [data[0], v, data[2]];
+        break;
+      case 3:
+        rgb = [data[0], data[1], v];
+        break;
+      case 4:
+        rgb = [data[2], data[0], v];
+        break;
+      default:
+        rgb = [v, data[0], data[1]];
+        break;
+    }
+  }
+  return '#' + rgb.map(function(x){
+    return ("0" + Math.round(x*255).toString(16)).slice(-2);
+  }).join('');
+};
