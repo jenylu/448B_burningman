5226e7af69a4a0c733cdc7184ab64eda17dc9756
Christina Kao <chris18@stanford.edu>
2016-05-29 17:12:43 -0700

    Switched to Semantic UI for better layout

--- index.js
+++ index.js
@@ -13,134 +13,146 @@ var years = ["2009", "2010", "2011", "2012", "2013", "2014", "2015"];
 //     $(this).toggleClass("selectedCol");
 // }); 
 
-d3.json("datasets/bm.json", function(error, data) {
+showWordCount("category");
 
-    if (error) {
-        return console.warn(error);
-    }
+function showWordCount(technique) {
 
-    for (j = 0; j < years.length; j ++) {
-    	console.log(j);
-    	year = years[j];
-    	data2 = data.filter(function(d) { return d.Year == year && d['NLP Technique'] == default_technique; })
+	d3.json("datasets/bm.json", function(error, data) {
 
-	    for (i = 0; i < data2.length; i ++) {
-	        object = data2[i];
-	        word = object['Word'];
-	     
-	        var div = document.getElementById('bm_' + year);
-	        //console.log(div.innerHTML);
-
-	        $('#bm_' + year).append('<span class=' + word +'>'+word+" "+'</span>');
-	        //div.innerHTML = div.innerHTML + word + "\n";
+	    if (error) {
+	        return console.warn(error);
 	    }
-    }
 
-});
+	    for (j = 0; j < years.length; j ++) {
+	    	console.log(j);
+	    	year = years[j];
+	    	data2 = data.filter(function(d) { return d.Year == year && d['NLP Technique'] == technique; })
 
-d3.json("datasets/event.json", function(error, data) {
+		    for (i = 0; i < data2.length; i ++) {
+		        object = data2[i];
+		        word = object['Word'];
+		        count = object['Count'];
+		        sentiment_scale = object['Sentiment Scale'];
+		        //console.log(word, count, sentiment_scale);
 
-    if (error) {
-        return console.warn(error);
-    }
+		        if (word.length < 100) {
+		        	var div = document.getElementById('bm_' + year);
+			        //console.log(div.innerHTML);
 
-    for (j = 0; j < years.length; j ++) {
-    	console.log(j);
-    	year = years[j];
-    	data2 = data.filter(function(d) { return d.Year == year && d['NLP Technique'] == default_technique; })
-
-	    for (i = 0; i < data2.length; i ++) {
-	        object = data2[i];
-	        word = object['Word'];
-	     
-	        var div = document.getElementById('event_' + year);
-	        //console.log(div.innerHTML);
-
-	        $('#event_' + year).append('<span class=' + word +'>'+word+" "+'</span>');
-	        //div.innerHTML = div.innerHTML + word + "\n";
+			        $('#bm_' + year).append('<span class=' + word +'>['+word+"] "+'</span>');
+			        //div.innerHTML = div.innerHTML + word + "\n";
+		        }
+		    }
 	    }
-    }
 
-});
+	});
 
-d3.json("datasets/art.json", function(error, data) {
+	d3.json("datasets/event.json", function(error, data) {
 
-    if (error) {
-        return console.warn(error);
-    }
-
-    for (j = 0; j < years.length; j ++) {
-    	console.log(j);
-    	year = years[j];
-    	data2 = data.filter(function(d) { return d.Year == year && d['NLP Technique'] == default_technique; })
-
-	    for (i = 0; i < data2.length; i ++) {
-	        object = data2[i];
-	        word = object['Word'];
-	     
-	        var div = document.getElementById('art_' + year);
-	        //console.log(div.innerHTML);
-
-	        $('#art_' + year).append('<span class=' + word +'>'+word+" "+'</span>');
-	        //div.innerHTML = div.innerHTML + word + "\n";
+	    if (error) {
+	        return console.warn(error);
 	    }
-    }
 
-});
+	    for (j = 0; j < years.length; j ++) {
+	    	console.log(j);
+	    	year = years[j];
+	    	data2 = data.filter(function(d) { return d.Year == year && d['NLP Technique'] == technique; })
 
-d3.json("datasets/camp.json", function(error, data) {
+		    for (i = 0; i < data2.length; i ++) {
+		        object = data2[i];
+		        word = object['Word'];
+		     
+		        var div = document.getElementById('event_' + year);
+		        //console.log(div.innerHTML);
 
-    if (error) {
-        return console.warn(error);
-    }
-
-    for (j = 0; j < years.length; j ++) {
-    	console.log(j);
-    	year = years[j];
-    	data2 = data.filter(function(d) { return d.Year == year && d['NLP Technique'] == default_technique; })
-
-	    for (i = 0; i < data2.length; i ++) {
-	        object = data2[i];
-	        word = object['Word'];
-	     
-	        var div = document.getElementById('camp_' + year);
-	        //console.log(div.innerHTML);
-
-	        $('#camp_' + year).append('<span class=' + word +'>'+word+" "+'</span>');
-	        //div.innerHTML = div.innerHTML + word + "\n";
+		        $('#event_' + year).append('<span class=' + word +'>['+word+"] "+'</span>');
+		        //div.innerHTML = div.innerHTML + word + "\n";
+		    }
 	    }
-    }
 
-});
+	});
 
-d3.json("datasets/twitter.json", function(error, data) {
+	d3.json("datasets/art.json", function(error, data) {
 
-    if (error) {
-        return console.warn(error);
-    }
-
-    for (j = 0; j < years.length; j ++) {
-    	console.log(j);
-    	year = years[j];
-    	data2 = data.filter(function(d) { return d.Year == year && d['NLP Technique'] == default_technique; })
-
-	    for (i = 0; i < data2.length; i ++) {
-	        object = data2[i];
-	        word = object['Word'];
-	     
-	        var div = document.getElementById('twitter_' + year);
-	        //console.log(div.innerHTML);
-
-	        $('#twitter_' + year).append('<span class=' + word +'>'+word+" "+'</span>');
-	        //div.innerHTML = div.innerHTML + word + "\n";
+	    if (error) {
+	        return console.warn(error);
 	    }
-    }
 
-});
+	    for (j = 0; j < years.length; j ++) {
+	    	console.log(j);
+	    	year = years[j];
+	    	data2 = data.filter(function(d) { return d.Year == year && d['NLP Technique'] == technique; })
+
+		    for (i = 0; i < data2.length; i ++) {
+		        object = data2[i];
+		        word = object['Word'];
+		     
+		        var div = document.getElementById('art_' + year);
+		        //console.log(div.innerHTML);
+
+		        $('#art_' + year).append('<span class=' + word +'>['+word+"] "+'</span>');
+		        //div.innerHTML = div.innerHTML + word + "\n";
+		    }
+	    }
+
+	});
+
+	d3.json("datasets/camp.json", function(error, data) {
+
+	    if (error) {
+	        return console.warn(error);
+	    }
+
+	    for (j = 0; j < years.length; j ++) {
+	    	console.log(j);
+	    	year = years[j];
+	    	data2 = data.filter(function(d) { return d.Year == year && d['NLP Technique'] == technique; })
+
+		    for (i = 0; i < data2.length; i ++) {
+		        object = data2[i];
+		        word = object['Word'];
+		     
+		        var div = document.getElementById('camp_' + year);
+		        //console.log(div.innerHTML);
+
+		        $('#camp_' + year).append('<span class=' + word +'>['+word+"] "+'</span>');
+		        //div.innerHTML = div.innerHTML + word + "\n";
+		    }
+	    }
+
+	});
+
+	d3.json("datasets/twitter.json", function(error, data) {
+
+	    if (error) {
+	        return console.warn(error);
+	    }
+
+	    for (j = 0; j < years.length; j ++) {
+	    	console.log(j);
+	    	year = years[j];
+	    	data2 = data.filter(function(d) { return d.Year == year && d['NLP Technique'] == default_technique; })
+
+		    for (i = 0; i < data2.length; i ++) {
+		        object = data2[i];
+		        word = object['Word'];
+		     
+		        var div = document.getElementById('twitter_' + year);
+		        //console.log(div.innerHTML);
+
+		        $('#twitter_' + year).append('<span class=' + word +'>['+word+"] "+'</span>');
+		        //div.innerHTML = div.innerHTML + word + "\n";
+		    }
+	    }
+
+	});
+
+}
 
 
 $(".itemHeader").on("click", function() {
-    $(this).parent().toggleClass("selectedCol");
+    //$(this).parent().toggleClass("selectedCol");
+    $(this).toggleClass("selectedCol");
 }); 
 
 $(".legend").on("click", function() {
